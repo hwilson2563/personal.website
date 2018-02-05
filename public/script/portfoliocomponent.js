c85477c2b11e4af1b5910d5e975e9afc2ddafbe8
Hilary Claire <hilary.wilson2563@gmail.com>
2018-02-05 13:33:53 -0500

    two slide shows working independantly

--- public/script/portfoliocomponent.js
+++ public/script/portfoliocomponent.js
@@ -3,8 +3,10 @@
 		templateUrl:"portfolio.html",
 		controller: function () {
 			var vm = this;
+			slideIndexOne = 1;
 			slideIndex = 1;
 			showSlides(slideIndex);
+			showSlidesOne(slideIndexOne);
 
 			vm.plusSlides = function plusSlides(n) {
   			showSlides(slideIndex += n);
@@ -14,15 +16,35 @@
   			showSlides(slideIndex = n);
 			}
 			function showSlides(n) {
-  			var i;
-  			var slides = document.getElementsByClassName("mySlides");
+   			var slides = document.getElementsByClassName("mySlides");
   			var dots = document.getElementsByClassName("dot");
   			if (n > slides.length) {slideIndex = 1}    
   			if (n < 1) {slideIndex = slides.length}
-  			for (i = 0; i < slides.length; i++) {
+  			for (var i = 0; i < slides.length; i++) {
       		slides[i].style.display = "none";  
   			}
-  			for (i = 0; i < dots.length; i++) {
+  			for (var i = 0; i < dots.length; i++) {
+      		dots[i].className = dots[i].className.replace(" active", "");
+  			}
+  			slides[slideIndex-1].style.display = "block";  
+  			dots[slideIndex-1].className += " active";
+			}
+			vm.plusSlidesOne = function plusSlides(n) {
+  			showSlidesOne(slideIndex += n);
+			}
+
+			vm.currentSlideOne = function currentSlide(n) {
+  			showSlidesOne(slideIndex = n);
+			}
+			function showSlidesOne(n) {
+   			var slides = document.getElementsByClassName("mySlidesOne");
+  			var dots = document.getElementsByClassName("dotOne");
+  			if (n > slides.length) {slideIndex = 1}    
+  			if (n < 1) {slideIndex = slides.length}
+  			for (var i = 0; i < slides.length; i++) {
+      		slides[i].style.display = "none";  
+  			}
+  			for (var i = 0; i < dots.length; i++) {
       		dots[i].className = dots[i].className.replace(" active", "");
   			}
   			slides[slideIndex-1].style.display = "block";  
